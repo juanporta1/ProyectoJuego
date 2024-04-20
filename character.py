20c8bdf179e73f190ac8c52a990cba2a47ebb4ed
JuanPorta14 <juanporta.c@gmail.com>
2024-04-20 15:51:43 -0300

    creacion de animaciones

--- character.py
+++ character.py
@@ -1,10 +1,40 @@
 import pygame
+import consts
 
 class Character():
     
-    def __init__(self, x, y):
-        self.shape = pygame.Rect(0, 0 , 20, 20)
+    def __init__(self, x, y,animation_sprites):
+        self.flip = False
+        self.animaton_srpites = animation_sprites
+        self.frame_index = 0
+        self.image = animation_sprites[self.frame_index]
+        self.update_time = pygame.time.get_ticks()
+        self.shape = pygame.Rect(0, 0 , consts.CHARACTER_WIDTH, consts.CHARACTER_HEIGHT)
         self.shape.center = (x, y)
         
+        
+    def move(self, delta_x, delta_y):
+        if delta_x > 0: 
+            self.flip = False
+            
+        elif delta_x < 0:
+            self.flip = True
+        
+        self.shape.x += delta_x
+        self.shape.y += delta_y
+    
+    def update_animation(self):
+        cooldown_animation = 150
+        self.image = self.animaton_srpites[self.frame_index]
+        
+        if (pygame.time.get_ticks() - self.update_time) >= cooldown_animation:
+            if self.frame_index == len(self.animaton_srpites) - 1:
+                self.frame_index = 0
+            else:    
+                self.frame_index += 1 
+            self.update_time = pygame.time.get_ticks()
+        
     def draw(self, window):
+        self.image_flip = pygame.transform.flip(self.image, flip_x=self.flip, flip_y=False)
+        window.blit(self.image_flip, self.shape)
         pygame.draw.rect(window, (255,255,0), self.shape)
--- main.py
+++ main.py
@@ -3,19 +3,84 @@ import consts
 from character import Character
 
 pygame.init()
-player = Character(x = 50, y = 50)
-
-window = pygame.display.set_mode((consts.WIDTH_WINDOW, consts.HEIGHT_WINDOW))
+window = pygame.display.set_mode((consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
 pygame.display.set_caption("Juego")
+
+
+character_walk_sprites = []
+
+def scale_image(image, scale):
+    width = image.get_width()
+    height = image.get_height()
+    new_image = pygame.transform.scale(image,( width * scale, height * scale))
+    return new_image    
+
+for i in range(6):
+    image = pygame.image.load(f"assets//images//characters//player//walk//Player_Walk_{i}.png")
+    image = scale_image(image,consts.CHARACTER_SCALE)
+    character_walk_sprites.append(image)
+
+player_image = pygame.image.load("assets//images//characters//player//walk//Player_Walk_0.png")
+player_image = scale_image(player_image,consts.CHARACTER_SCALE)
+#Controlar el frame rate
+clock = pygame.time.Clock()
+#Definicion de variables
+
+player = Character(x = 100,
+                   y = 100,
+                   animation_sprites=character_walk_sprites)
 run = True
+move_left = False
+move_right = False
+move_up = False
+move_down = False    
 
 while run:
+    window.fill(consts.WINDOW_COLORBG)
+    
+    clock.tick(consts.FPS)
+
+    delta_x = 0
+    delta_y = 0
+    
+    #COntrolar movimiento del jugador
+    if move_left == True:
+        delta_x = -consts.CHARACTER_VELOCITY
+    elif move_right == True:
+        delta_x = consts.CHARACTER_VELOCITY
+    
+    if move_up == True:
+        delta_y = -consts.CHARACTER_VELOCITY
+    elif move_down == True:
+        delta_y = consts.CHARACTER_VELOCITY
     
     player.draw(window)
-    
+    player.move(delta_x, delta_y)
+    player.update_animation()
+   
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run = False  
+            run = False 
+            
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                move_left = True
+            if event.key == pygame.K_d:
+                move_right = True
+            if event.key == pygame.K_w:
+                move_up = True
+            if event.key == pygame.K_s:
+                move_down = True
+        
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                move_left = False
+            if event.key == pygame.K_d:
+                move_right = False
+            if event.key == pygame.K_w:
+                move_up = False
+            if event.key == pygame.K_s:
+                move_down = False
             
     pygame.display.update()
 pygame.quit()
